5ce7c6214055fbf60bba5953afdcae768d99302b
Sebastian Popa <sebastianpopa208@gmail.com>
2024-12-14 01:09:05 +0200

    _tryToMatchOrder

--- contracts/market/Market.sol
+++ contracts/market/Market.sol
@@ -22,6 +22,8 @@ contract Market is Ownable {
     mapping(BetOutcome => mapping(address => uint)) freeShares;
     mapping(BetOutcome => mapping(address => uint)) reservedShares;
 
+    mapping(BetOutcome => BetOutcome) oppositeBetOutcome;
+
     constructor(
         address _owner,
         uint16 _decimals,
@@ -33,6 +35,9 @@ contract Market is Ownable {
         granularity = _granularity;
         title = _title;
         description = _description;
+
+        oppositeBetOutcome[BetOutcome.Yes] = BetOutcome.No;
+        oppositeBetOutcome[BetOutcome.No] = BetOutcome.Yes;
     }
 
     /**
@@ -59,6 +64,12 @@ contract Market is Ownable {
 
         orderBook[_outcome][OrderSide.Buy][_price].push(order);
         userActiveOrdersCount[user]++;
+
+        _tryToMatchBuyOrder(
+            _outcome,
+            _price,
+            orderBook[_outcome][OrderSide.Buy][_price].length - 1
+        );
     }
 
     function placeLimitSellOrder(
@@ -84,5 +95,116 @@ contract Market is Ownable {
         reservedShares[_outcome][user] += _shares;
 
         orderBook[_outcome][OrderSide.Sell][_price].push(order);
+        userActiveOrdersCount[user]++;
+
+        _tryToMatchSellOrder(
+            _outcome,
+            _price,
+            orderBook[_outcome][OrderSide.Sell][_price].length - 1
+        );
+    }
+
+    function _tryToMatchBuyOrder(
+        BetOutcome _outcome,
+        uint _price,
+        uint _orderIndex
+    ) private {
+        Order storage order = orderBook[_outcome][OrderSide.Buy][_price][
+            _orderIndex
+        ];
+
+        //match buy order with sell order
+        for (
+            uint _tryPrice = 0;
+            _tryPrice <= _price && order.remainingShares > 0;
+            _tryPrice++
+        ) {
+            for (
+                uint _tryIndexInOB = 0;
+                _tryIndexInOB <
+                orderBook[_outcome][OrderSide.Sell][_tryPrice].length &&
+                    order.remainingShares > 0;
+                _tryIndexInOB++
+            ) {
+                Order storage _tryOrder = orderBook[_outcome][OrderSide.Sell][
+                    _tryPrice
+                ][_tryIndexInOB];
+
+                if (_tryOrder.isActive) {
+                    uint _matchedShares = order.remainingShares <
+                        _tryOrder.remainingShares
+                        ? order.remainingShares
+                        : _tryOrder.remainingShares;
+
+                    //execute direct trade
+                    // also do -=rem_shares
+                }
+            }
+        }
+
+        // match buy order with buy order of opposite outcome
+        BetOutcome _oppositeOutcome = oppositeBetOutcome[_outcome];
+        uint _oppositePrice = 10 ** decimals - _price;
+
+        for (
+            uint _tryIndexInOB = 0;
+            _tryIndexInOB <
+            orderBook[_oppositeOutcome][OrderSide.Buy][_oppositePrice].length &&
+                order.remainingShares > 0;
+            _tryIndexInOB++
+        ) {
+            Order storage _tryOrder = orderBook[_oppositeOutcome][
+                OrderSide.Buy
+            ][_oppositePrice][_tryIndexInOB];
+
+            if (_tryOrder.isActive) {
+                uint _matchedShares = order.remainingShares <
+                    _tryOrder.remainingShares
+                    ? order.remainingShares
+                    : _tryOrder.remainingShares;
+
+                //execute generating trade
+                // also do -=rem_shares
+            }
+        }
+    }
+
+    function _tryToMatchSellOrder(
+        BetOutcome _outcome,
+        uint _price,
+        uint _orderIndex
+    ) private {
+        Order storage order = orderBook[_outcome][OrderSide.Sell][_price][
+            _orderIndex
+        ];
+
+        //match sell order with buy order
+        for (
+            uint _tryPrice = 10 ** decimals;
+            _tryPrice >= _price && order.remainingShares > 0;
+            _tryPrice--
+        ) {
+            for (
+                uint _tryIndexInOB = 0;
+                _tryIndexInOB <
+                orderBook[_outcome][OrderSide.Buy][_tryPrice].length &&
+                    order.remainingShares > 0;
+                _tryIndexInOB++
+            ) {
+                Order storage _tryOrder = orderBook[_outcome][OrderSide.Buy][
+                    _tryPrice
+                ][_tryIndexInOB];
+
+                if (_tryOrder.isActive) {
+                    uint _matchedShares = order.remainingShares <
+                        _tryOrder.remainingShares
+                        ? order.remainingShares
+                        : _tryOrder.remainingShares;
+
+                    //execute direct trade
+                    // also do -=rem_shares
+                }
+            }
+        }
     }
 }
