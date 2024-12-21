c4526811b10a3b273fd0b338d89458a99a0f0adf
Sebastian Popa <sebastianpopa208@gmail.com>
2024-12-21 11:05:24 +0200

    fixed getActiveOrders script

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -76,9 +76,11 @@ PUBLIC_ADDRESS=0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199 npx hardhat run script
 # sell order
 PRIVATE_KEY=df57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e OUTCOME=0 PRICE=400000 SHARES=5 npx hardhat run scripts/placeLimitSellOrder.js --network localhost
 
+
+PUBLIC_ADDRESS=0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199 OUTCOME=0 SIDE=1 npx hardhat run scripts/getActiveOrders.js --network localhost
+
 PRIVATE_KEY=de9be858da4a475276426320d5e9262ecfc3ba460bfac56360bfa6c4c28b4ee0 OUTCOME=0 SIDE=0 SHARES=2 npx hardhat run scripts/placeMarketOrder.js --network localhost
 
-exit 0
 PUBLIC_ADDRESS=0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199 npx hardhat run scripts/getPositions.js --network localhost
 
 # Get the positions
--- scripts/getActiveOrders.js
+++ scripts/getActiveOrders.js
@@ -17,16 +17,17 @@ const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 // Create contract instance
 const contract = new ethers.Contract(contractAddress, contractABI, wallet);
 
-async function getActiveOrders(eventIndex, marketIndex, betOutcome, orderSide) {
+async function getActiveOrders(eventIndex, marketIndex, betOutcome, orderSide, user) {
   try {
     console.log("Fetching active orders...");
 
     // Call the getActiveOrders function
-    const activeOrders = await contract.getActiveOrders(eventIndex, marketIndex, betOutcome, orderSide);
+    const activeOrders = await contract.getActiveOrders(eventIndex, marketIndex, betOutcome, orderSide, user);
 
     console.log("Active orders retrieved successfully!");
-    console.log(activeOrders); // Print the retrieved orders
+    // console.log(activeOrders); // Print the retrieved orders
     return activeOrders;
+ 
   } catch (error) {
     console.error("Error fetching active orders:", error);
     throw error;
@@ -42,11 +43,19 @@ const betOutcome = process.env.OUTCOME; // Directly from environment
 const orderSide = process.env.SIDE; // Directly from environment
 
 // Call getActiveOrders with hardcoded indices and environment variables
-const user = process.env.PUBLIC_KEY;
+const user = process.env.PUBLIC_ADDRESS;
 
 getActiveOrders(eventIndex, marketIndex, betOutcome, orderSide, user)
   .then((orders) => {
-    console.log("Orders:", orders);
+     orders.forEach((order, index) => {
+    console.log(`Order ${index + 1}:`);
+    console.log(`  User: ${order.user}`);
+    console.log(`  Initial Shares: ${order.initialShares}`);
+    console.log(`  Remaining Shares: ${order.remainingShares}`);
+    console.log(`  Timestamp: ${new Date(order.timestamp * 1000).toLocaleString()}`);
+    console.log(`  Is Active: ${order.isActive}`);
+    console.log(`  Current Total Price: ${order.currentTotalPrice}`);
+  });
   })
   .catch((error) => {
     console.error("Error:", error);
